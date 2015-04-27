3dcc31cc4859f23ac348efdcb158f1305a530670
Tangent Chang <tien.jen.chang@sv.cmu.edu>
2015-04-26 23:29:40 -0700

    finalized script

--- yelp_recommender_evaluation.R
+++ yelp_recommender_evaluation.R
@@ -17,15 +17,15 @@ yelp_rating_flat = yelp_review[,c("user_id","business_id","stars")]
 yelp_rating_unique = yelp_rating_flat[!duplicated(yelp_rating_flat[1:2]),]
 
 # select reviews of only the most reviewed businesses
-topNReviewedBusinesses = 50
-business_review_frequency = data.frame(table(yelp_rating_unique$business_id))
-top_n_most_reviewed_businesses = business_review_frequency[order(-business_review_frequency$Freq),][1:topNReviewedBusinesses,]
-yelp_rating_unique = yelp_rating_unique[yelp_rating_unique$business_id %in% top_n_most_reviewed_businesses[,1], ]
+# topNReviewedBusinesses = 247
+# business_review_frequency = data.frame(table(yelp_rating_unique$business_id))
+# top_n_most_reviewed_businesses = business_review_frequency[order(-business_review_frequency$Freq),][1:topNReviewedBusinesses,]
+# yelp_rating_unique = yelp_rating_unique[yelp_rating_unique$business_id %in% top_n_most_reviewed_businesses[,1], ]
 
 # filter users who have made less than x reviews
 #TODO: how does min user review affect evaluation?
-minUserReviews = 3
 # change minUserReviews from 10 to 3, since dataset is smaller now.
+minUserReviews = 10
 user_ids <- table(yelp_rating_unique$user_id)
 yelp_rating_unique = yelp_rating_unique[yelp_rating_unique$user_id %in% names(user_ids)[user_ids >= minUserReviews], ]
 
@@ -43,11 +43,6 @@ r = as(m, "realRatingMatrix")
 # rm(m)
 # this is a good place to save your workspace!!
 
-#algorithms = list("random" = list(name="RANDOM", param=NULL), "popular" = list(name="POPULAR", param=NULL), "user-based CF" = list(name="UBCF", param=list(method="Cosine", nn=50, minRating=5)))
-	# TODO: UBCF
-	# TODO: IBCF
-	# TODO: others
-
 var_nn = 50
 algorithms = list(
 	"random" = list(name="RANDOM", param=NULL),
@@ -58,12 +53,14 @@ algorithms = list(
 	"ibcf-c" = list(name="IBCF", param=list(method="Cosine")),
 	"ibcf-p" = list(name="IBCF", param=list(method="Pearson")),
 	"ibcf-j" = list(name="IBCF", param=list(method="Jaccard")),
-	"ubcf-c-centered" = list(name="UBCF", param=list(method="Cosine", normalize="center", nn=var_nn)),
-	"ubcf-p-centered" = list(name="UBCF", param=list(method="Pearson", normalize="center", nn=var_nn)),
-	"ubcf-j-centered" = list(name="UBCF", param=list(method="Jaccard", normalize="center", nn=var_nn)),
-	"ibcf-c-centered" = list(name="IBCF", param=list(method="Cosine", normalize="center")),
-	"ibcf-p-centered" = list(name="IBCF", param=list(method="Pearson", normalize="center")),
-	"ibcf-j-centered" = list(name="IBCF", param=list(method="Jaccard", normalize="center")),
+	# ibcf's default is center, no need to assign, or we can try "Z-score"
+	# "ubcf-c-centered" = list(name="UBCF", param=list(method="Cosine", normalize="center", nn=var_nn)),
+	# "ubcf-p-centered" = list(name="UBCF", param=list(method="Pearson", normalize="center", nn=var_nn)),
+	# "ubcf-j-centered" = list(name="UBCF", param=list(method="Jaccard", normalize="center", nn=var_nn)),
+	# ibcf's default is center, no need to assign, or we can try "Z-score"
+	# "ibcf-c-centered" = list(name="IBCF", param=list(method="Cosine", normalize="center")),
+	# "ibcf-p-centered" = list(name="IBCF", param=list(method="Pearson", normalize="center")),
+	# "ibcf-j-centered" = list(name="IBCF", param=list(method="Jaccard", normalize="center")),
 	"svd" = list(name="SVD", param=list(categories = 20, method="Cosine", normalize = "center", normalize_sim_matrix = FALSE, alpha = 0.5, treat_na = "median", minRating = NA))
 	#"pca" = list(name="PCA", param=list(categories = 20, method="Cosine", normalize = "center", normalize_sim_matrix = FALSE, alpha = 0.5, na_as_zero = FALSE, minRating = NA))
 )
@@ -82,23 +79,31 @@ kFold_evals = evaluate(kFold, algorithms, n=c(1, 3, 5, 10, 15, 20))
 
 # evaluation results
 CFM_random = getConfusionMatrix(kFold_evals[["random"]])
-CFM_popular =getConfusionMatrix(kFold_evals[["popular"]])
-CFM_ubcf_c =getConfusionMatrix(kFold_evals[["ubcf-c"]])
-CFM_ubcf_p =getConfusionMatrix(kFold_evals[["ubcf-p"]])
-CFM_ubcf_j =getConfusionMatrix(kFold_evals[["ubcf-j"]])
-CFM_ibcf_c =getConfusionMatrix(kFold_evals[["ibcf-c"]])
-CFM_ibcf_p =getConfusionMatrix(kFold_evals[["ibcf-p"]])
-CFM_ibcf_j =getConfusionMatrix(kFold_evals[["ibcf-j"]])
-CFM_ubcf_c =getConfusionMatrix(kFold_evals[["ubcf-c-centered"]])
-CFM_ubcf_p =getConfusionMatrix(kFold_evals[["ubcf-p-centered"]])
-CFM_ubcf_j =getConfusionMatrix(kFold_evals[["ubcf-j-centered"]])
-CFM_ibcf_c =getConfusionMatrix(kFold_evals[["ibcf-c-centered"]])
-CFM_ibcf_p =getConfusionMatrix(kFold_evals[["ibcf-p-centered"]])
-CFM_ibcf_j =getConfusionMatrix(kFold_evals[["ibcf-j-centered"]])
+CFM_popular = getConfusionMatrix(kFold_evals[["popular"]])
+CFM_ubcf_c = getConfusionMatrix(kFold_evals[["ubcf-c"]])
+CFM_ubcf_p = getConfusionMatrix(kFold_evals[["ubcf-p"]])
+CFM_ubcf_j = getConfusionMatrix(kFold_evals[["ubcf-j"]])
+CFM_ibcf_c = getConfusionMatrix(kFold_evals[["ibcf-c"]])
+CFM_ibcf_p = getConfusionMatrix(kFold_evals[["ibcf-p"]])
+CFM_ibcf_j = getConfusionMatrix(kFold_evals[["ibcf-j"]])
+# CFM_ubcf_c =getConfusionMatrix(kFold_evals[["ubcf-c-centered"]])
+# CFM_ubcf_p =getConfusionMatrix(kFold_evals[["ubcf-p-centered"]])
+# CFM_ubcf_j =getConfusionMatrix(kFold_evals[["ubcf-j-centered"]])
+# CFM_ibcf_c =getConfusionMatrix(kFold_evals[["ibcf-c-centered"]])
+# CFM_ibcf_p =getConfusionMatrix(kFold_evals[["ibcf-p-centered"]])
+# CFM_ibcf_j =getConfusionMatrix(kFold_evals[["ibcf-j-centered"]])
 CFM_svd =getConfusionMatrix(kFold_evals[["svd"]])
 #CFM_pca =getConfusionMatrix(kFold_evals[["pca"]])
 
 #Get average value of k-fold
+avg(kFold_evals[["random"]])
+avg(kFold_evals[["popular"]])
+avg(kFold_evals[["ubcf-c"]])
+avg(kFold_evals[["ubcf-p"]])
+avg(kFold_evals[["ubcf-j"]])
+avg(kFold_evals[["ibcf-c"]])
+avg(kFold_evals[["ibcf-p"]])
+avg(kFold_evals[["ibcf-j"]])
 avg(kFold_evals[["svd"]])
 
 #x=random_avg[,"precision"]
@@ -111,10 +116,14 @@ avg(kFold_evals[["svd"]])
 
 # visualization
 # ROC Curve
+pdf("Desktop/Recommender/pic/roc_min=10.pdf")
 plot(kFold_evals, annotate=1:length(algorithms), legend="topleft")
+dev.off()
 # can't draw, since each CFM contains NaN. After adjusting minUserReviews and goodRating we get the diagram.
 # plot prec/recall chart
+pdf("Desktop/Recommender/pic/pr_min=10.pdf")
 plot(kFold_evals, "prec/rec", annotate=1:length(algorithms), legend="topleft")
+dev.off()
 # TODO: Evaluate Predictions (RMSE)
 # TODO: Evaluate Binarized data
 # TODO: Build a script that can make a prediction (top 5) and retrieve the restaurant names for a user
